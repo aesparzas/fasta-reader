b1bcc0b67da36d0cd70b262c4fe8c01d3000d002
Adolfo Esparza <esparzasordoadolfo@gmail.com>
2019-11-04 15:28:11 -0600

    Initial coommit

---
+++ README.md
@@ -0,0 +1,27 @@
+# FASTA Read Library
+
+Library installable by '''pip install pip install fastaread-1.0-py3-none-any.whl''' made for aptude selection process
+
+## Getting started
+I recomend you to install this library in a virtual enviroment, the creation process is described berlow. All temporary files and/or folders are ignored
+
+```
+mkdir tmp
+```
+
+```
+python3 -m venv tmp/venv
+```
+
+```
+. tmp/venv/vin/activate
+```
+
+```
+pip install pip install dist/fastaread-1.0-py3-none-any.whl
+```
+	
+## Authors
+
+* **Adolfo Esparza** - *Initial work* - [aesparzas](https://github.com/aesparzas)
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fastaread',
+    version='1.0',
+    scripts=['fasta-read'],
+    author="Adolfo Esparza",
+    author_email="esparzasordoadolfo@gmail.com",
+    description="A small library for reading FASTA files",
+    long_description=long_description,
+    url="https://github.com/aesparzas/fasta-reader",
+    packages=setuptools.find_packages(),
+    install_requires=['biopython'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
